ce687a604cc5391964697ba5f4a5fa09ac45723a
dbyilla69 <doranyilla@gmail.com>
2019-02-05 10:24:11 -0600

    set up apollo on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from "react-apollo";
+//components
+import BabyList from './components/baby-list';
 
+//apollo client setup
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ApolloProvider client={client}>
+      <div id='main'>
+        <h1>Baby List</h1>
+        <BabyList/>
       </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ client/src/components/baby-list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BabyList extends Component {
+    render() {
+        return (
+            <div>
+                <li id='baby-list'>
+                <ul>Baby Name</ul>
+            </li>
+            </div>
+        );
+    }
+}
+
+export default BabyList;
